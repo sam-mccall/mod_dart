989a9c6ae0085a62287a314ce187eae618744b6f
Sam McCall <sammccall@google.com>
2012-05-23 18:41:25 +0200

    Include stdlib

--- src/mod_dart.c
+++ src/mod_dart.c
@@ -1,4 +1,9 @@
-#include "dart_api.h"
+#ifndef DEBUG
+#define NDEBUG
+#endif
+
+#include "include/dart_api.h"
+#include "bin/builtin.h"
 
 #include "httpd.h"
 #include "http_config.h"
@@ -6,14 +11,20 @@
 #include "http_protocol.h"
 #include "ap_config.h"
 
-#define WARN(r, ...) ap_log_error(APLOG_MARK, LOG_WARNING, 0, (r)->server, __VA_ARGS__)
+#define AP_WARN(r, ...) ap_log_error(APLOG_MARK, LOG_WARNING, 0, (r)->server, __VA_ARGS__)
 
-const uint8_t* snapshot_buffer = 0;
+extern const uint8_t* snapshot_buffer;
 
 static bool IsolateCreate(const char* name, void* data, char** error) {
   Dart_Isolate isolate = Dart_CreateIsolate(name, snapshot_buffer, data, error);
   if (!isolate) return false;
   Dart_EnterScope();
+  Builtin::SetNativeResolver(Builtin::kBuiltinLibrary);
+  Builtin::SetNativeResolver(Builtin::kCryptoLibrary);
+  Builtin::SetNativeResolver(Builtin::kIOLibrary);
+  Builtin::SetNativeResolver(Builtin::kJsonLibrary);
+  Builtin::SetNativeResolver(Builtin::kUriLibrary);
+  Builtin::SetNativeResolver(Builtin::kUtfLibrary);
   return true;
 }
 
@@ -22,27 +33,44 @@ static bool IsolateInterrupt() {
   return true;
 }
 
+static Dart_Handle LoadFile(Dart_Handle path) {
+  const char* cpath;
+  Dart_Handle result = Dart_StringToCString(path, &cpath);
+  if (Dart_IsError(result)) return result;
+  return Dart_NewString("quick brown fox!");
+}
+
+static Dart_Handle LibraryTagHandler(Dart_LibraryTag type, Dart_Handle library, Dart_Handle url, Dart_Handle import_map) {
+  if (type == kCanonicalizeUrl) return url;
+  if (type == kLibraryTag) return Dart_Null();
+  Dart_Handle source = LoadFile(url);
+  if (Dart_IsError(source)) return source;
+  if (type == kImportTag) return Dart_LoadLibrary(url, source, import_map);
+  return Dart_LoadSource(library, url, source);
+}
+
 static bool dart_vm_init(request_rec *r) {
   // TODO threadsafe
   static int initialized = 0;
   static int initializeState = 0;
   if (initialized) {
-    if (!initializeState) WARN(r, "Previously failed to initialize dart VM");
+    if (!initializeState) AP_WARN(r, "Previously failed to initialize dart VM");
     return initializeState;
   }
   initialized = 1;
   initializeState = Dart_SetVMFlags(0, NULL) && Dart_Initialize(IsolateCreate, IsolateInterrupt);
-  if (!initializeState) WARN(r, "Failed to initialize dart VM");
+  if (!initializeState) AP_WARN(r, "Failed to initialize dart VM");
   return initializeState;
 }
 
 static bool dart_isolate_create(request_rec *r) {
   char* error;
   if (!IsolateCreate("name", NULL, &error)) {
-    WARN(r, "Failed to create isolate: %s", error);
+    AP_WARN(r, "Failed to create isolate: %s", error);
     return false;
   }
   Dart_EnterScope();
+  Dart_SetLibraryTagHandler(LibraryTagHandler);
   return true;
 }
 
@@ -52,10 +80,9 @@ static void dart_isolate_destroy(request_rec *r) {
 }
 
 static Dart_Handle dart_library_create(request_rec *r) {
-  return Dart_LoadScript(Dart_NewString("main"), Dart_NewString("main() => 42;"), Dart_Null());
+  return Dart_LoadScript(Dart_NewString("main"), Dart_NewString("#import('dart:io');\nmain() => stdout.writeString('hello\\n');"), Dart_Null());
 }
 
-/* The sample content handler */
 static int dart_handler(request_rec *r) {
   if (strcmp(r->handler, "dart")) {
     return DECLINED;
@@ -64,15 +91,18 @@ static int dart_handler(request_rec *r) {
   if (!dart_isolate_create(r)) return HTTP_INTERNAL_SERVER_ERROR;
   Dart_Handle library = dart_library_create(r);
   if (Dart_IsError(library)) {
-    WARN(r, "Failed to load library: %s", Dart_GetError(library));
+    AP_WARN(r, "Failed to load library: %s", Dart_GetError(library));
     dart_isolate_destroy(r);
     return HTTP_INTERNAL_SERVER_ERROR;
   }
   Dart_Handle result = Dart_Invoke(library, Dart_NewString("main"), 0, NULL);
   if (Dart_IsError(result)) {
-    WARN(r, "Failed to execute main(): %s", Dart_GetError(result));
+    AP_WARN(r, "Failed to execute main(): %s", Dart_GetError(result));
+    r->content_type = "text/plain";
+    r->status = HTTP_INTERNAL_SERVER_ERROR;
+    ap_rprintf(r, "Failed to execute main(): %s\n", Dart_GetError(result));
     dart_isolate_destroy(r);
-    return HTTP_INTERNAL_SERVER_ERROR;
+    return OK;
   }
   dart_isolate_destroy(r);
 
