c3ccd992624792d8fc235e431c67b09f9f2c8a70
Sam McCall <sammccall@google.com>
2012-08-22 13:01:49 +0200

    And fix build with latest SDK on mac

--- build.sh
+++ build.sh
@@ -12,10 +12,11 @@ type "$HTTPD" || exit "Couldn't find httpd, edit build.config"
 
 UNAME=`uname`
 if [[ "$UNAME" == "Darwin" ]]; then
-  DART_LIB=$DART_SRC/xcodebuild/${DART_MODE}$DART_ARCH
-  DART_GEN=$DART_SRC/xcodebuild/DerivedSources/${DART_MODE}$DART_ARCH
+  DART_LIB=$DART_SRC/runtime/xcodebuild/${DART_MODE}$DART_ARCH
+  DART_GEN=$DART_SRC/runtime/xcodebuild/DerivedSources/${DART_MODE}$DART_ARCH
   LIBRARY_GROUP_START= # not needed - mac linker 'just works' with circular dependencies
   LIBRARY_GROUP_END=
+  WEB_GEN="$DART_LIB/../dart-runtime.build/${DART_MODE}$DART_ARCH/dart.build/Objects-normal/*/web_gen.o"
 else
   if [[ "$UNAME" != "Linux" ]]; then
     echo "Warning: Unrecognized OS $UNAME, this likely won't work"
@@ -24,6 +25,7 @@ else
   DART_GEN=$DART_SRC/out/${DART_MODE}$DART_ARCH/obj/gen
   LIBRARY_GROUP_START=,--start-group # dart static libraries have circular references
   LIBRARY_GROUP_END=,--end-group
+  WEB_GEN=$DART_LIB/../dart/gen/web_gen.o
 fi
 
 if [[ "$DART_MODE" == "Debug" ]]; then
@@ -33,8 +35,8 @@ else
 fi
 
 rm src/mod_dart_gen.c; python $DART_SRC/runtime/tools/create_string_literal.py --output src/mod_dart_gen.c --include 'none' --input_cc src/mod_dart_gen.c.tmpl --var_name "mod_dart_source" src/mod_dart.dart
-$APXS -S CC=g++ -c $COPTS -o mod_dart.so -Wc,-Wall -Wc,-Werror -I $DART_SRC/runtime -lstdc++ -I $DART_GEN \
--Wl,-Wl$LIBRARY_GROUP_START,$DART_LIB/libdart_export.a,$DART_LIB/libdart_builtin.a,$DART_LIB/libdart_lib_withcore.a,$DART_LIB/libdart_vm.a,$DART_LIB/libjscre.a,$DART_LIB/libdouble_conversion.a,$DART_LIB/../dart/gen/web_gen.o$LIBRARY_GROUP_END \
+LTFLAGS="--tag=CC" $APXS -S CC=g++ -c $COPTS -o mod_dart.so -Wc,-Wall -Wc,-Werror -I $DART_SRC/runtime -lstdc++ -I $DART_GEN \
+-Wl,-Wl$LIBRARY_GROUP_START,$DART_LIB/libdart_export.a,$DART_LIB/libdart_builtin.a,$DART_LIB/libdart_lib_withcore.a,$DART_LIB/libdart_vm.a,$DART_LIB/libjscre.a,$DART_LIB/libdouble_conversion.a,$WEB_GEN$LIBRARY_GROUP_END \
 src/builtin.c src/mod_dart_gen.c src/apache_library.c src/mod_dart.c && \
 sudo $APXS -i -a -n dart mod_dart.la && \
 sudo apachectl restart
